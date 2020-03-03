35232697e487bf22b174b6d09832c529bab2f943
Alex Zhukovich <zhukovich.a@gmail.com>
2020-03-03 18:57:15 +0100

    Cleaned up CoffeeDrinkListActivity class

--- app/src/main/java/com/alexzh/coffeedrinks/feature/list/CoffeeDrinkListActivity.kt
+++ app/src/main/java/com/alexzh/coffeedrinks/feature/list/CoffeeDrinkListActivity.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import com.alexzh.coffeedrinks.R
 import com.alexzh.coffeedrinks.feature.detail.CoffeeDrinkDetailActivity
 import com.alexzh.coffeedrinks.feature.detail.CoffeeDrinkDetailFragment
+import com.alexzh.coffeedrinks.feature.list.model.CoffeeDrinkUiModel
 import kotlinx.android.synthetic.main.activity_coffee_drinks_list.*
 import kotlinx.android.synthetic.main.item_list.*
 import org.koin.android.ext.android.inject
@@ -15,57 +16,63 @@ import org.koin.android.ext.android.inject
 class CoffeeDrinkListActivity : AppCompatActivity() {
 
     private val viewModel: CoffeeDrinkListViewModel by inject()
-
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
-    private var twoPane: Boolean = false
+    private val adapter: CoffeeDrinksAdapter by lazy {
+        CoffeeDrinksAdapter(
+            isTabletMode,
+            ::onCoffeeDrinkClicked,
+            ::onCoffeeDrinkFavouriteClicked
+        )
+    }
+    private var isTabletMode: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coffee_drinks_list)
 
         setSupportActionBar(toolbar)
-
         if (item_detail_container != null) {
-            twoPane = true
+            isTabletMode = true
         }
+        setUpCoffeeDrinkList()
 
-        // TODO: item_list == recyclerView
-        val adapter = CoffeeDrinksAdapter(
-            twoPane,
-            { coffeeDrinkUiModel ->
-                if (twoPane) {
-                    val fragment = CoffeeDrinkDetailFragment().apply {
-                        arguments = Bundle().apply {
-                            putLong(CoffeeDrinkDetailFragment.COFFEE_DRINK_ID, coffeeDrinkUiModel.id)
-                        }
-                    }
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.item_detail_container, fragment)
-                        .commit()
-                } else {
-                    val intent = Intent(this@CoffeeDrinkListActivity, CoffeeDrinkDetailActivity::class.java).apply {
-                        putExtra(CoffeeDrinkDetailFragment.COFFEE_DRINK_ID, coffeeDrinkUiModel.id)
-                    }
-                    startActivity(intent)
-                }
-            },
-            { coffeeDrinkUiModel ->
-                viewModel.updateFavouriteState(coffeeDrinkUiModel)
-            }
-        )
+        viewModel.getCoffeeDrinks().observe(this, Observer {
+            showCoffeeDrinks(it)
+        })
+
+        viewModel.loadCoffeeDrinks()
+    }
+
+    private fun setUpCoffeeDrinkList() {
         recyclerView.adapter = adapter
         recyclerView.addItemDecoration(
             DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL)
         )
+    }
 
-        viewModel.getCoffeeDrinks().observe(this, Observer {
-            adapter.setCoffeeDrinks(it)
-        })
+    private fun showCoffeeDrinks(coffeeDrinks: List<CoffeeDrinkUiModel>) {
+        adapter.setCoffeeDrinks(coffeeDrinks)
+    }
 
-        viewModel.loadCoffeeDrinks()
+    private fun onCoffeeDrinkClicked(coffeeDrink: CoffeeDrinkUiModel) {
+        if (isTabletMode) {
+            val fragment = CoffeeDrinkDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putLong(CoffeeDrinkDetailFragment.COFFEE_DRINK_ID, coffeeDrink.id)
+                }
+            }
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.item_detail_container, fragment)
+                .commit()
+        } else {
+            val intent = Intent(this@CoffeeDrinkListActivity, CoffeeDrinkDetailActivity::class.java).apply {
+                putExtra(CoffeeDrinkDetailFragment.COFFEE_DRINK_ID, coffeeDrink.id)
+            }
+            startActivity(intent)
+        }
+    }
+
+    private fun onCoffeeDrinkFavouriteClicked(coffeeDrink: CoffeeDrinkUiModel) {
+        viewModel.updateFavouriteState(coffeeDrink)
     }
 }
